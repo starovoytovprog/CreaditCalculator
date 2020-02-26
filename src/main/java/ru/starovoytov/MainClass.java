670873f584c0f01d995bfb55ffb22d41e165912a
Anton Starovoytov <adminofusa@gmail.com>
2020-02-26 08:19:39 +0300

    Инициализация проекта gradle, Hello World!

---
+++ src/main/java/ru/starovoytov/MainClass.java
@@ -0,0 +1,13 @@
+package ru.starovoytov;
+
+/**
+ * Стартовый класс
+ *
+ * @author anton_starovoytov
+ * @since 2020.02.26
+ */
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
